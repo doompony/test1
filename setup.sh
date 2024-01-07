12ceceda31b4decbd40673a1a54f587aa272d03d
Doompony Lewis <doompony.links@pm.me>
2024-01-07 13:09:43 -0600

    change setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 git config --add --local user.name "Doompony Lewis"
-
-git config --add --local user.email "doompony@pype.aleeas.com"
-
+git config --add --local user.email "doompony.links@pm.me"
 git config --add --local core.sshCommand 'ssh -i ~/.ssh/doompony_github'
